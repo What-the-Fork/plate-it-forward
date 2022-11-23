04ef41012af3281b68f58c297ef973721a51a099
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-11-22 17:17:41 -0700

    created Home directory and added components, as well as completed navbar (minus our logo)

--- frontend/src/ui/App.jsx
+++ frontend/src/ui/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
-import { Home } from './Home.jsx'
+import { Home } from './Home/Home.jsx'
 import { FourOhFour } from './FourOhFour.jsx'
+import {Navigation} from "./Shared/Components/Navigation";
 
 
 export function App() {
     return (
         <>
             <BrowserRouter>
+                <Navigation />
                 <Routes>
                     <Route  path='/' element={<Home />} />
                     <Route path={"*"} element={<FourOhFour />} />
---
+++ frontend/src/ui/Shared/Components/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Container, Nav, Navbar} from "react-bootstrap";
+import NavbarToggle from "react-bootstrap/NavbarToggle";
+import NavbarCollapse from "react-bootstrap/NavbarCollapse";
+import {Link} from "react-router-dom";
+
+export function Navigation () {
+    return (
+        <Navbar bg={'light'} expand={'lg'} variant={'light'}>
+            <Container fluid>
+                {/* PUT LOGO HERE WHEN READY */}
+                <Navbar.Brand><Link to={'/'}>PIF</Link></Navbar.Brand>
+                <NavbarToggle aria-controls={'navbarScroll'} />
+                <NavbarCollapse id={'navbarScroll'}>
+                    {/* MS AUTO PUSHES LINKS TO RIGHT */}
+                    <Nav className={'ms-auto'}>
+                        <Link to={'/community-center'} className={'nav-link'}>Community Centers</Link>
+                        <Link to={'/partner-with-us'} className={'nav-link'}>Partner with Us</Link>
+                        <Link to={'/partner-signup'} className={'nav-link'}>Partner Portal</Link>
+                    </Nav>
+                </NavbarCollapse>
+            </Container>
+        </Navbar>
+    )
+}
