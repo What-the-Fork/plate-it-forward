fb1c4cf5a6f5b04e50a44a9d2384b39f86a9f8e0
Erin Ruiz <eruiz24@cnm.edu>
2022-11-25 21:38:30 -0700

    progress on donation inline form

--- frontend/src/ui/Profile/RestaurantUser/Donations.jsx
+++ frontend/src/ui/Profile/RestaurantUser/Donations.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import {Container, Form, InputGroup} from "react-bootstrap";
+import {Container, Form, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 export function Donations() {
     return (
         <>
-            <Container>
-                <h5>Donation Form</h5>
-            <Form.Group className="inline-form" controlId="formBasicEmail">
-                <Form.Label>Meals Donated</Form.Label>
-                <Form.Control type="email" placeholder="Enter Number of Meals Donated" />
-            </Form.Group>
+            <Container className={'bg-light rounded p-3 mt-4 mb-5 border border-secondary'}>
+            <Form className="form-inline mt-3">
+                <h5 className={'text-center'}>Donation Form</h5>
+                <Container className={''}>
+                <Row className="form-row justify-content-center mt-3">
+                    <div className="col-auto">
+                <Form.Label className="sr-only" htmlFor="inlineFormInputName2">Restaurant Name</Form.Label>
+                <input type="text" className="form-control mb-2 mr-sm-2"
+                       placeholder="Restaurant A"></input></div>
+                    <div className="col-auto">
+                <Form.Label className="sr-only" htmlFor="inlineFormInputName2">Number of Meals Donated</Form.Label>
+                <input type="text" className="form-control mb-2 mr-sm-2"
+                       placeholder="45"></input></div>
+                    <div className="col-auto">
+                <Form.Label className="sr-only" htmlFor="inlineFormInputName2">Donation Date</Form.Label>
+                <input type="text" className="form-control mb-2 mr-sm-2"
+                       placeholder="MM/DD/YYYY"></input></div>
+                    <div className="col-auto">
+                        <Button type="submit" className="btn btn-primary align-middle">Submit</Button></div>
+                </Row>
+                </Container>
+            </Form>
             </Container>
         </>
     )
--- frontend/src/ui/Profile/RestaurantUser/PartnershipRequest.jsx
+++ frontend/src/ui/Profile/RestaurantUser/PartnershipRequest.jsx
@@ -4,16 +4,9 @@ import { Col, Container, Row } from "react-bootstrap";
 export function PartnershipRequest() {
     return (
         <>
-            <Container fluid={true} className={'bg-light py-5'}>
+            <Container fluid={true} className={'bg-light p-5'}>
                 <h5 className={'text-center'}>Partner with a Community Center</h5>
-                <Row className={'gx-md-5 gy-5'}>
-                    <Col>
 
-                    </Col>
-                    <Col>
-
-                    </Col>
-                </Row>
             </Container>
         </>
     )
