29747c851fc5a43c690404b4f9b6e458a43e16ce
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-11-28 15:34:32 -0700

    deleted second image and paragraph

--- frontend/src/ui/Partner/Partner.jsx
+++ frontend/src/ui/Partner/Partner.jsx
@@ -15,15 +15,6 @@ export function Partner() {
                             <p>Bacon ipsum dolor amet frankfurter andouille ground round t-bone cupim pork chop. Tail pork cupim porchetta drumstick shoulder chuck salami burgdoggen beef ribs pork belly beef. Flank andouille pork chop, bresaola fatback prosciutto shankle ball tip.</p>
                         </Col>
                     </Row>
-                    <Row className={'gx-md-1 gy-5 mt-4 mb-4'}>
-                        <Col md={5} className={'text-center order-md-last order-lg-last'}>
-                            <Image fluid={true} src={'https://www.placecage.com/c/325/325'} alt={'Nick Cage doing Nick Cage things'}/>
-                        </Col>
-                        <Col md={7} className={'fs-5 order-md-first order-lg-first'}>
-                            <h3>What We Do</h3>
-                            <p>Ribeye jowl rump, swine shoulder ball tip picanha fatback chuck filet mignon sirloin kevin porchetta. Capicola short ribs sirloin, filet mignon corned beef ham rump hamburger. Tail t-bone leberkas shankle buffalo swine ball tip bresaola drumstick sirloin pastrami porchetta.</p>
-                        </Col>
-                    </Row>
                     {/* NEEDS ALL (EXCEPT PADDING AND MB) TO BE CENTERED */}
                     <Button className={'p-4 mx-auto d-flex justify-content-center my-5'}>Become a partner today!</Button>
             </Container>
