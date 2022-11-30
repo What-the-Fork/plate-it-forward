1de25f1da2652db5010cb890da48070890f10ebe
Sergio Salas-Villalobos <ssalas28@cnm.edu>
2022-11-29 23:08:27 -0700

    start of map component for center directory page

--- frontend/src/ui/Center/Center.jsx
+++ frontend/src/ui/Center/Center.jsx
@@ -1,11 +1,19 @@
 import React, {useEffect} from "react"
 import {CenterHeader} from "./CenterHeader.jsx";
 import {CenterDirectory} from "./CenterDirectory.jsx";
-import {Map} from "./Map.jsx";
+import {Map} from "react-map-gl";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchAllCenters} from "../../store/centers.js";
+import {Container, Row} from "react-bootstrap";
 
 export function Center() {
+
+    const [points] = React.useState([
+        { lat: 35.332, lng: -106.652 },
+        { lat: 35.339, lng: -106.656 },
+        { lat: 35.40, lng: -106.666 },
+        { lat: 35.23, lng: -106.4444 }
+    ])
 
     const centers = useSelector(state => state.centers ? state.centers : [])
 
@@ -21,7 +29,22 @@ export function Center() {
     return (
         <>
             <CenterHeader />
-            <Map />
+            <Container>
+                <Row className={'justify-content-center'}>
+                    <h1>Center Map</h1>
+                    <Map
+                        initialViewState={{
+                    latitude: 35.33,
+                    longitude: -106.65,
+                    zoom: 9
+                        }}
+                        mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+                        style={{ width: 600, height: 400 }}
+                        mapStyle="mapbox://styles/mapbox/dark-v9"
+                        >
+                        {points.map((point, index) => <Pin lat={point.lat} lng={point.lng} index={index} key={index} />)}
+                    </Map>
+                </Row>
             <CenterDirectory centers={centers}/>
         </>
     )
