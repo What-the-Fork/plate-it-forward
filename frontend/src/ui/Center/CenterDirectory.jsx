ff48e05691cbe8e355a3ecc1bb087e12dec48dd9
Sergio Salas-Villalobos <ssalas28@cnm.edu>
2022-12-01 10:36:38 -0700

    commit test

--- frontend/src/ui/Center/CenterDirectory.jsx
+++ frontend/src/ui/Center/CenterDirectory.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import {CenterListing} from "./CenterListing.jsx";
-
+// commit test
 export const CenterDirectory = (props) => {
     const {centers} = props
-
 
     return (
         <>
