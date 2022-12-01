58e431ad0f6d8e682ddf881dc3b3f2cb45950396
Sergio Salas-Villalobos <ssalas28@cnm.edu>
2022-11-30 17:50:35 -0700

    made changes to center info file, inserted placeholder text until redux

--- frontend/src/ui/Profile/CenterUser/CenterInfo.jsx
+++ frontend/src/ui/Profile/CenterUser/CenterInfo.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import {Col, Container, Image, Row} from "react-bootstrap";
 
-export function CenterInfo({ce}) {
+export function CenterInfo() {
     return (
         <>
             <section className={'py-5'}>
                 <Container>
                     <Row className={'gx-md-5 gy-5'}>
                         <Col md={6} className={'text-center order-lg-2'}>
-                            <Image fluid={true} className={'rounded rounded-circle'} src={'https://www.placecage.com/c/200/200'} alt={'Nick Cage doing Nick Cage things'}/>
+                            <Image fluid={true} className={'rounded rounded-circle'} src={'http://placekitten.com/200/200'} alt={'Nick Cage doing Nick Cage things'}/>
                         </Col>
                         <Col md={6} className={'fs-5 order-lg-1'}>
-                            <h3>{centerName}</h3>
-                            <p>{centerContactName}</p>
-                            <p>{centerContactEmail}</p>
+                            <h3>Jackie Daytona Volleyball Comm. Center</h3>
+                            <p>Laszlo Cravensworth</p>
+                            <p>communityrelations@jdcommcenter.com</p>
                             <p>(717) 309-3793</p>
                             <p>137 New Holland Ave, Lancaster, PA 17602</p>
                         </Col>
