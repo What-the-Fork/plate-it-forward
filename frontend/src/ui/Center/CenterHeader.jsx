fdd68ad92f17887afb16840ed7aaec5c39190980
Erin <ruize04@gmail.com>
2022-12-07 14:33:10 -0700

    adjusted length of subheader in center header section

--- frontend/src/ui/Center/CenterHeader.jsx
+++ frontend/src/ui/Center/CenterHeader.jsx
@@ -6,7 +6,7 @@ export function CenterHeader () {
         <>
             <section className={'bg-light'}>
             <h1 className={'text-center pt-4 display-3'}><strong>Community Centers</strong></h1>
-                <p className={'text-center pb-4 border-bottom'}>Find a community center near you! Please note that community centers have different restaurant partners and operating hours. Please contact the center nearest you to learn more about their meal service.</p>
+                <p className={'text-center pb-4 border-bottom'}>Find a community center near you! Please contact the center nearest you to learn more about their meal services and operating hours.</p>
             </section>
         </>
     )
